88ee6b8f0350f150b9744e2cd8d5e0cb031a88b4
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2023-12-11 09:04:50 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'DODP-DATI';
+  title:string = 'CGM - Controladoria Geral do Município';
 }
